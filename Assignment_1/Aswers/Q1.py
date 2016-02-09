c219fb0a0125ec3c5e998ca7e4f059b483829629
Zhenyu Peng <pzy927@gmail.com>
2016-02-09 00:24:48 -0500

    organized

---
+++ Assignment_1/Aswers/Q1.py
@@ -0,0 +1,1 @@
+import pymongofrom bson.son import SONclient = pymongo.MongoClient()db = client['Movies_database']movies = db.moviesratings = db.ratingstags = db.tags# Q1cursor = db.movies.find({"Title": {'$regex': 'copycat', '$options': 'i'}})for doc in cursor:    print(doc['Genres'])
